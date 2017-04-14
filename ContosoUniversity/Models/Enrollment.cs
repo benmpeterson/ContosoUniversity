11f876c87ff4079e00bee5d7780ba71db7edcbcf
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-14 16:55:38 -0400

    Implemented Entity Framework Data Model and Basic CRUD Functionality

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        //? indicates that Grade property is nullable. A grade that's null is different from a zero grade
+        //null means a grade ins't known or hasn't been assigned yet
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        [Required(ErrorMessage ="Please Enter A Valid Date *This is a custom Error Message")]
+        public DateTime EnrollmentDate { get; set; }
+
+        //This will hold all the Enrollements of a created instance of this Student class
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
