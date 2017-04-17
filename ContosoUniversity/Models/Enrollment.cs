c05a57d3bed62cade4bc48b8ee9832e4da158aca
Ben Peterson <ben.micah.peterson@gmail.com>
2017-04-17 10:57:22 -0400

    Completed School Data Model

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public enum Grade
     {
@@ -12,6 +14,7 @@
         public int StudentID { get; set; }
         //? indicates that Grade property is nullable. A grade that's null is different from a zero grade
         //null means a grade ins't known or hasn't been assigned yet
+        [DisplayFormat(NullDisplayText = "No Grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
